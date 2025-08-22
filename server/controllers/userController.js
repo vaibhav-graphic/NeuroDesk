673e783c5acc8f58a787181d1742d96ce493df10
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-08-22 13:12:12 +0530

    add login User

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
 
 import User from "../models/User.js";
 
@@ -21,8 +22,29 @@ export const registerUser = async (req, res) => {
 
         const user = await User.create({name, email, password});
 
-        const token = generateToken(user._id);
-        res.status(200).json({succes: true, token});
+        res.status(200).json({succes: true, msg: 'User register succesfully'});
+    }
+    catch(error){
+        res.status(500).json({succes: false, msg: 'Server error'});
+    }
+}
+
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(isMatch){
+                const token = generateToken(user._id);
+                res.status(200).send({succes: true, token});
+            }
+        }
+
+        res.status(400).json({succes: false, msg: 'Invalid email or password'});
     }
     catch(error){
         res.status(500).json({succes: false, msg: 'Server error'});
