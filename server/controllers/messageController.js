3c0718160d4ce7ed359ff589c553bb8cfa58f83a
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-08-22 21:53:19 +0530

    add image generation feature

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,20 @@
+import axios from "axios";
+
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
+import imagekit from "../configs/imageKit.js";
 
 export const textMessageController = async (req, res) => {
   try {
     const userId = req.user._id;
+
+    if (req.user.credits < 1) {
+      res.status(200).json({
+        success: false,
+        msg: "You  don't have enough credits to use this feature",
+      });
+    }
+
     const { chatId, prompt } = req.body;
 
     const chat = await Chat.findOne({ userId, _id: chatId });
@@ -14,7 +25,7 @@ export const textMessageController = async (req, res) => {
       isImage: false,
     });
 
-    const {choices} = await openai.chat.completions.create({
+    const { choices } = await openai.chat.completions.create({
       model: "gemini-2.0-flash",
       messages: [
         {
@@ -24,14 +35,77 @@ export const textMessageController = async (req, res) => {
       ],
     });
 
-    const reply = {...choices[0].message, timestamps: Date.now, isImage: false};
-    res.json({success: true, reply});
+    const reply = {
+      ...choices[0].message,
+      timestamps: Date.now,
+      isImage: false,
+    };
+    res.json({ success: true, reply });
 
     chat.messages.push(reply);
     await chat.save();
-    await User.updateOne({_id: userId}, {$inc: {credits: -1}});
-
+    await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
   } catch (error) {
-    res.json({success: false, msg: error.message});
+    res.json({ success: false, msg: error.message });
+  }
+};
+
+export const imageMessageController = async () => {
+  try {
+    const userId = req.user._id;
+
+    if (req.user.credits < 2) {
+      res.status(200).json({
+        success: false,
+        msg: "You  don't have enough credits to use this feature",
+      });
+    }
+
+    const { prompt, chatId, isPublished } = req.body;
+    const chat = await Chat.findOne({ userId, _id: chatId });
+
+    chat.messages.push({
+      role: "user",
+      content: prompt,
+      timestamps: Date.now(),
+      isImage: true,
+    });
+
+    const encodedPrompt = encodeURIComponent(prompt);
+
+    const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}
+    /ik-genimg-prompt-${encodedPrompt}/NeuroDesk/${Date.now()}.png?tr=w-800,h-800`;
+
+    const aiImageResponse = await axios.get(generatedImageUrl, {
+      responseType: "arraybuffer",
+    });
+
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      aiImageResponse.data,
+      "binary"
+    ).toString("base64")}`;
+
+    const uploadResponse = await imagekit.upload({
+        file: base64Image,
+        fileName: `${Date.now()}.png`,
+        folder: "NeuroDesk"
+    });
+
+    const reply = {
+        role: "assistant",
+        content: uploadResponse.url,
+        timestamps: Date.now(),
+        isImage: true,
+        isPublished
+    }
+
+    res.status(400).json({success: true, reply});
+
+    chat.messages.push(reply);
+    await chat.save();
+
+    await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
+  } catch (error) {
+    return res.status(400).json({success: false, msg: error.message});
   }
 };
